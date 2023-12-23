e78ba16b8dc03266f21e51153dbfb98572ef23b2
edugichuki <muriithigichuki854@gmail.com>
2023-12-23 18:01:20 +0300

    Edit App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ import PlacePage from "./pages/PlacePage";
 import PlacesFormPage from "./pages/PlacesFormPage";
 import PlacesPage from "./pages/PlacesPage";
 import ProfilePage from "./pages/ProfilePage";
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
